0a24736fa1205babe4a227f390aac1b900531654
Marcela Malaver <marcela9409@gmail.com>
2020-07-28 11:59:51 -0500

    useFetch con The Breaking Bad API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import {HookApp} from './HookApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
