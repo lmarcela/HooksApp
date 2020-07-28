2319fa82f444aa46184193a0b3bc46626e360838
Marcela Malaver <marcela9409@gmail.com>
2020-07-28 11:27:10 -0500

    Probar un formulario con CustomHook

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
-    const [coordenadas, setCoordenadas] = useState({x: 0, y:0});
-    const {x,y} = coordenadas;
+  const [coordenadas, setCoordenadas] = useState({ x: 0, y: 0 });
+  const { x, y } = coordenadas;
 
-    useEffect(() => {
-        const mouseMove = (e) => {
-            const coordenadas = {x: e.x, y:e.y};
-            setCoordenadas(coordenadas);
-        }
+  useEffect(() => {
+    const mouseMove = (e) => {
+      const coordenadas = { x: e.x, y: e.y };
+      setCoordenadas(coordenadas);
+    };
 
-        window.addEventListener('mousemove',mouseMove);
-        
-        return () => {
-            window.removeEventListener('mousemove',mouseMove);
-        }
-    }, [])
+    window.addEventListener("mousemove", mouseMove);
 
-    return (
-        <div>
-            <h3>Awesome!</h3>
-            <p>x: {x}, y:{y}</p>
-        </div>
-    )
-}
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Awesome!</h3>
+      <p>
+        x: {x}, y:{y}
+      </p>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import {HookApp} from './HookApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-    <SimpleForm />,
+    <FormWithCustomHook />,
   document.getElementById('root')
 );
