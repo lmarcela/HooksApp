1e51e7d081e09f7eea6c7039e40ba91dbf60b61e
Marcela Malaver <marcela9409@gmail.com>
2020-07-27 19:41:56 -0500

    Creacion de CustomHook SimpleForm (usa useState, useEffect)

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css'
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        name:'',
+        email:''
+    })
+
+    const {name, email} = formState;
+    //useEffect generico
+    useEffect(() => {
+        console.log("hey")
+    }, [])
+
+    //useEffect para el form
+    useEffect(() => {
+        console.log("formState changed!")
+    }, [formState])
+
+    //useEffect para email
+    useEffect(() => {
+        console.log("email changed!")
+    }, [email])
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        });
+    }
+    return (
+        <>
+           <h2>useEffect</h2> 
+           <hr/>
+           <div className="form-group">
+                <input type="text" name="name" className="form-control" placeholder="Tu nombre" autoComplete="off" value={name} onChange={handleInputChange} />
+           </div>
+           
+           <div className="form-group">
+                <input type="text" name="email" className="form-control" placeholder="Tu correo" autoComplete="off" value={email} onChange={handleInputChange} />
+           </div>
+        </>
+    )
+}
