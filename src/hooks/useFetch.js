d608ad7ca4c732db9c26f1eea673c12614324e03
Marcela Malaver <marcela9409@gmail.com>
2020-07-28 18:49:29 -0500

    useRef en useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react";
 
 export const useFetch = (url) => {
-    
-    const [state, setState] = useState({data:null, loading:true, error:null})
-    useEffect(() => {
-        setState({data:null, loading:true, error:null});
-        fetch(url).then(resp => resp.json()).then(data=>{
-            setState({
-                loading:false,
-                error:null,
-                data:data
-            })
-        })
-    }, [url])
-    return state;
-}
+  const estaMontado = useRef(true);
+
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      estaMontado.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (estaMontado.current) {
+          setState({
+            loading: false,
+            error: null,
+            data: data,
+          });
+        }
+      });
+  }, [url]);
+  return state;
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import {HookApp} from './HookApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 ReactDOM.render(
-    <FocusScreen />,
+    <RealExampleRef />,
   document.getElementById('root')
 );
