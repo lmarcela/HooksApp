9a6f5aa12a54131e0c7e2983d4cc167f2d7a0a65
Marcela Malaver <marcela9409@gmail.com>
2020-07-27 20:06:27 -0500

    Optimizacion de retorno de useEffect

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+    const [coordenadas, setCoordenadas] = useState({x: 0, y:0});
+    const {x,y} = coordenadas;
+
     useEffect(() => {
-        console.log("Componente montado")
+        const mouseMove = (e) => {
+            const coordenadas = {x: e.x, y:e.y};
+            setCoordenadas(coordenadas);
+        }
+
+        window.addEventListener('mousemove',mouseMove);
+        
         return () => {
-            console.log("Componente desmontado")
+            window.removeEventListener('mousemove',mouseMove);
         }
     }, [])
-    
+
     return (
         <div>
             <h3>Awesome!</h3>
+            <p>x: {x}, y:{y}</p>
         </div>
     )
 }
