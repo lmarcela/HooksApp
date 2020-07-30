5631f2a3ee423657df627496029f47b3af19f696
Marcela Malaver <marcela9409@gmail.com>
2020-07-30 14:00:59 -0500

    useCallBack en Padre.js y React.memo en Hijo.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import {HookApp} from './HookApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
@@ -13,8 +13,9 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { Memorize } from './components/06-memos/Memorize';
 //import { MemoHook } from './components/06-memos/MemoHook';
+//import { CallbackHook } from './components/06-memos/CallbackHook';
 
 ReactDOM.render(
-    <CallbackHook />,
+    <Padre />,
   document.getElementById('root')
 );
