3167b375f0b30664a87fd7a93303cf239f75e26b
Marcela Malaver <marcela9409@gmail.com>
2020-08-03 19:55:49 -0500

    pruebas en HomeScreen

---
+++ src/tests/09-useContext/HomeScreen.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { mount } from "enzyme";
+import "@testing-library/jest-dom";
+import { HomeScreen } from "../../components/09-useContext/HomeScreen";
+import { UserContext } from "../../components/09-useContext/UserContext";
+
+describe("Pruebas en <HomeScreen />", () => {
+  const user = {
+    name: "Marcela",
+    email: "marcela@gmail.com",
+  };
+  const wrapper = mount(
+    <UserContext.Provider
+      value={{
+        user,
+      }}
+    >
+      <HomeScreen />
+    </UserContext.Provider>
+  );
+
+  test("debe de mostrarse correctamente", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
